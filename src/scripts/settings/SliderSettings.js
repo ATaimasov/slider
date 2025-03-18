a74cd8b9476c4d1b94292e8575aa7c92b9a1e2ae
Alexander Taimasov <136252899+ATaimasov@users.noreply.github.com>
2025-03-18 03:39:44 +0300

    minor fixes

--- src/scripts/settings/SliderSettings.js
+++ src/scripts/settings/SliderSettings.js
@@ -3,8 +3,8 @@ import { Slider } from "../services/Slider.js";
 const testSlider = new Slider({
   sliderContainer: "slider__test",
   isLoop: true,
-  hasInfinity: false,
   hasAutoSlide: true,
   shouldInterruptAutoSlide: false,
   autoSlideInterval: 5000,
+  hasBullets: false,
 });
