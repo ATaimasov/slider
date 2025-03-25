8036818338d901996810e80d3900f95a929de02a
Alexander Taimasov <136252899+ATaimasov@users.noreply.github.com>
2025-03-25 15:02:31 +0300

    minor fixes

--- src/scripts/settings/SliderSettings.js
+++ src/scripts/settings/SliderSettings.js
@@ -2,7 +2,7 @@ import { Slider } from "../services/Slider.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const testSlider = new Slider({
-    mode: "fade",
+    mode: "default",
     sliderContainer: "slider__test",
     isLoop: true,
     hasAutoSlide: false,
